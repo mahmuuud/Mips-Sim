659193c201e3e54b25a3c11c54d4da553490382d
root <root@localhost.localdomain>
2018-04-15 23:37:01 +0200

    edit an error

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,9 +1,12 @@
 package Main;
+import GUI7.GUI;
+import sun.awt.image.ToolkitImage;
 import Blocks.*;
 import Stages.*;
 public class main {
     public static void main(String[] args){
 
-
+        GUI on =new GUI();
+        on.setVisible(true);
     }
 }
